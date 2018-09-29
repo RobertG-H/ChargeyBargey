bd722103e4788edeb7b61be332f9b639bb7ffb63
unknown <robbie.meyer12@gmail.com>
2018-09-28 23:54:57 -0400

    I think this gets the bars working

---
+++ Assets/Scripts/Meters/MeterController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeterController : MonoBehaviour {
+
+	private RectTransform rectTransform;
+
+	private const int MAXLENGTH = 300;
+	private float length;
+	private float leftPos;
+	// Use this for initialization
+	void Start () {
+		length = MAXLENGTH;
+		rectTransform = GetComponent< RectTransform >();
+		leftPos = (int) rectTransform.localPosition.x - (int)(0.5 * length);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	public void SetLength (float percent) {
+		length = Mathf.Min(1.0f, (percent / 100.0f)) * (float) MAXLENGTH;
+		rectTransform.localScale = new Vector2(length, rectTransform.localScale.y);
+		rectTransform.localPosition = new Vector2(leftPos + length/2,rectTransform.localPosition.y);
+	}
+}
