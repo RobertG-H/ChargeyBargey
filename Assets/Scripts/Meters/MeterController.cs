896ad31febdd277c68139f10bb28c75c6c8b5528
unknown <robbie.meyer12@gmail.com>
2018-09-30 14:04:12 -0400

    meters blink when at 100

--- Assets/Scripts/Meters/MeterController.cs
+++ Assets/Scripts/Meters/MeterController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class MeterController : MonoBehaviour {
 
 	private RectTransform rectTransform;
+	private Renderer renderer;
 
 	private const int MAXLENGTH = 175;
 	private float length;
@@ -14,15 +15,28 @@ public class MeterController : MonoBehaviour {
 		length = MAXLENGTH;
 		rectTransform = GetComponent< RectTransform >();
 		leftPos = (int) rectTransform.localPosition.x - MAXLENGTH/2;
+		renderer = GetComponent<Renderer>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+	}
+
+	void Blink () {
+		renderer.enabled = !renderer.enabled;
 	}
 
 
 	public void SetLength (float percent) {
+		if (percent >= 100 && length < MAXLENGTH) {
+			InvokeRepeating("Blink", 0.4f - Time.unscaledTime % 0.4f, 0.2f);
+			Debug.Log(0.2f - Time.unscaledTime % 0.2f);
+			Debug.Log(Time.unscaledTime);
+		} else if (percent < 100 && length >= MAXLENGTH) {
+			CancelInvoke("Blink");
+			renderer.enabled = true;
+		}
+
 		length = Mathf.Min(1.0f, (percent / 100.0f)) * (float) MAXLENGTH;
 		rectTransform.localScale = new Vector2(length, rectTransform.localScale.y);
 		rectTransform.localPosition = new Vector2(leftPos + length/2, rectTransform.localPosition.y);
